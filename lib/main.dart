6739c53f43308906f84a9dbf19cfb9d0b03ed28f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-05-16 23:09:59 +0530

    feat: add method to build key

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,21 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        color: color,
+        child: null,
+      ),
+    );
   }
 
   @override
@@ -18,9 +29,7 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-
-            ],
+            children: <Widget>[],
           ),
         ),
       ),
