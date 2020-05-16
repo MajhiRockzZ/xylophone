7da808fb83e3c667d360879f29c66e5ab45b6a9a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-05-16 23:01:53 +0530

    feat: add play sound method

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
